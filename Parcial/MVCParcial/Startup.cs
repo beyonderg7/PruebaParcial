3239e08831b77abff755457d90c82ffdbfd6cc89
Christian Alejandro Yabeta <christianyabeta@gmail.com>
2019-03-30 10:19:44 -0400

    Commit MVC creado

---
+++ Parcial/MVCParcial/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCParcial.Startup))]
+namespace MVCParcial
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
