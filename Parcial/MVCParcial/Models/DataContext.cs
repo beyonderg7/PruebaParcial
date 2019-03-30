e12d827e49e2de414eceb70e68ad67e0ac4c8452
Christian Alejandro Yabeta <christianyabeta@gmail.com>
2019-03-30 10:58:59 -0400

    Commit con Modelo MVC

---
+++ Parcial/MVCParcial/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace MVCParcial.Models
+{
+    public class DataContext:DbContext
+    {
+
+
+        public DataContext():base("DefaultConnection")
+        {
+
+
+
+        }
+
+        public System.Data.Entity.DbSet<MVCParcial.Model.Product> Products { get; set; }
+    }
+}
